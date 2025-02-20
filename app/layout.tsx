7182e8fb296659b6ef656ec717e996294c57596a
JT <jtorres.inbox1@gmail.com>
2025-02-20 15:01:05 -0500

    google adsense addition

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google AdSense script */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6107860292413809"
+          crossorigin="anonymous"
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
